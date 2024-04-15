1915d51f5e8affd9e4a0a23b50bebfa8fc685a07
AyaIbrahim3027 <ayaibrahim3027@gmail.com>
2024-04-16 00:40:13 +0200

    creating models

--- lib/domain/models.dart
+++ lib/domain/models.dart
@@ -17,3 +17,24 @@ class SliderViewObject {
       this.currentIndex,
       );
 }
+
+// login models
+class Customer{
+  String id;
+  String name;
+  int numOfNotifications;
+  Customer(this.id,this.name,this.numOfNotifications);
+}
+
+class Contact{
+  String phone;
+  String email;
+  String link;
+  Contact(this.phone,this.email,this.link);
+}
+
+class Authentication{
+  Customer customer;
+  Contact contacts;
+  Authentication(this.customer,this.contacts);
+}
