aad8cfa787e4981e298c1abe70e230fae9c3cbd5
AyaIbrahim3027 <ayaibrahim3027@gmail.com>
2024-04-18 07:18:20 +0200

    add Response Message

--- lib/data/network/error_handler.dart
+++ lib/data/network/error_handler.dart
@@ -20,7 +20,7 @@ class ResponseCode{
   static const int BAD_REQUEST = 400; // failure, API rejected request
   static const int UNAUThORISED = 401; // failure, user is not authorised
   static const int FORBIDDEN = 403; // failure, API rejected request
-  // static const int NOT_FOUND = 200;
+  static const int NOT_FOUND = 404;
   static const int INTERNAL_SERVER_Error = 500; // failure, crash in server side
 
   // local status code
@@ -31,4 +31,22 @@ class ResponseCode{
   static const int CACHE_ERROR = -5;
   static const int NO_INTERNET_CONNECTION = -6;
   static const int UNKNOWN = -7;
+}
+
+class ResponseMessage{
+  static const String SUCCESS = "success"; // success with data
+  static const String NO_CONTENT = "success"; // success with no data
+  static const String BAD_REQUEST = "Bad request, Try again later"; // failure, API rejected request
+  static const String UNAUThORISED = "User ia unauthorised, Try again later"; // failure, user is not authorised
+  static const String FORBIDDEN = "Forbidden request, Try again later"; // failure, API rejected request
+  static const String INTERNAL_SERVER_Error = "Some thing went wrong, Try again later"; // failure, crash in server side
+
+  // local status code
+  static const String CONNECT_TIMEOUT = "Time out error, Try again later";
+  static const String CANCEL = "Request was cancelled, Try again later";
+  static const String RECIEVE_TIMEOUt = "Time out error, Try again later";
+  static const String SEND_TIMEOUT = "Time out error, Try again later";
+  static const String CACHE_ERROR = "Cache error, Try again later";
+  static const String NO_INTERNET_CONNECTION = "Please check your  internet connection";
+  static const String UNKNOWN = "Some thing went wrong, Try again later";
 }
