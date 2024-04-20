0c66cfaa520396f366a01a78698c067e72205113
AyaIbrahim3027 <ayaibrahim3027@gmail.com>
2024-04-20 13:27:14 +0200

    Login View UI

--- lib/presentation/login/view/login_view.dart
+++ lib/presentation/login/view/login_view.dart
@@ -1,5 +1,8 @@
+import 'package:advanced_flutter/presentation/resources/color_manager.dart';
+import 'package:advanced_flutter/presentation/resources/values_manager.dart';
 import 'package:flutter/material.dart';
 
+import '../../resources/assets_manager.dart';
 import '../viewmodel/login_viewmodel.dart';
 
 class LoginView extends StatefulWidget {
@@ -13,23 +16,63 @@ class _LoginViewState extends State<LoginView> {
   final LoginViewModel _viewModel = LoginViewModel(_loginUseCase);
   final TextEditingController _userNameController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
-
+  final _fromKey = GlobalKey<FormState>();
 
   _bind() {
     _viewModel.start(); // tell viewmodel, start your job
-    _userNameController.addListener(() => _viewModel.setUserName(_userNameController.text));
-    _passwordController.addListener(() => _viewModel.setPassword(_passwordController.text));
+    _userNameController
+        .addListener(() => _viewModel.setUserName(_userNameController.text));
+    _passwordController
+        .addListener(() => _viewModel.setPassword(_passwordController.text));
   }
 
   @override
   void initState() {
-      _bind();
+    _bind();
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
+  }
+
+  Widget _getContentWidget() {
+    return Scaffold(
+      body: Container(
+        padding: const EdgeInsets.only(top: AppPadding.p100),
+        color: ColorManager.white,
+        child: SingleChildScrollView(
+          child: Form(
+            key: _fromKey,
+            child:  Column(
+              children: [
+                const Center(
+                    child: Image(
+                        image: AssetImage(
+                  ImageAssets.splashLogo,
+                ))),
+                const SizedBox(
+                  height: AppSize.s28,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: AppPadding.p28),
+                  child: StreamBuilder<bool>(
+                    stream: _viewModel.outIsUserNameValid,
+                    builder: (context,snapshot){
+                      return TextFormField(
+                        keyboardType: TextInputType.emailAddress,
+                        controller: _userNameController,
+                      );
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 
   @override
