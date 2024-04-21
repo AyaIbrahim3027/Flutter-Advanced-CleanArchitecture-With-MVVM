e1fc0ec7002db3b79049c867365fa74c4495b9ab
AyaIbrahim3027 <ayaibrahim3027@gmail.com>
2024-04-21 10:22:06 +0200

    Add dependency injection Instances

--- lib/app/dependancy_injection.dart
+++ lib/app/dependancy_injection.dart
@@ -1,5 +1,13 @@
 import 'package:advanced_flutter/app/app_prefs.dart';
+import 'package:advanced_flutter/data/data_source/remote_data_source.dart';
+import 'package:advanced_flutter/data/network/app_api.dart';
+import 'package:advanced_flutter/data/network/dio_factory.dart';
+import 'package:advanced_flutter/data/network/network_info.dart';
+import 'package:advanced_flutter/data/repository/repository_implementer.dart';
+import 'package:advanced_flutter/domain/repository/repository.dart';
+import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 final instance = GetIt.instance;
@@ -8,11 +16,29 @@ Future<void> initAppModule() async {
 
   // shared prefs instance
   final sharedPrefs = await SharedPreferences.getInstance();
-  instance.registerLazySingleton<SharedPreferences>(()=> sharedPrefs);
+  instance.registerLazySingleton<SharedPreferences>(() => sharedPrefs);
 
   // app prefs instance
-  instance.registerLazySingleton<AppPreferences>(() => AppPreferences(instance()));
+  instance
+      .registerLazySingleton<AppPreferences>(() => AppPreferences(instance()));
+
+  // network info instance
+  instance.registerLazySingleton<NetworkInfo>(
+      () => NetworkInfoImpl(InternetConnectionChecker()));
+
+  // dio factory
+  instance.registerLazySingleton<DioFactory>(() => DioFactory(instance()));
+
+  Dio dio = await instance<DioFactory>().getDio();
+
+  // app service client
+  instance.registerLazySingleton<AppServiceClient>(() => AppServiceClient(dio));
+  
+  // remote data source
+  instance.registerLazySingleton<RemoteDataSource>(() => RemoteDataSourceImpl(instance()));
+
+  // repository instance
+  instance.registerLazySingleton<Repository>(() => RepositoryImpl(instance(), instance()));
 }
 
-Future<void> initLoginModule() async {
-}
+Future<void> initLoginModule() async {}
