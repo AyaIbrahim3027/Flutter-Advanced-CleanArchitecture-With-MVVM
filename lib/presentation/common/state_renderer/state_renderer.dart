b87230543a4edf66307c87841dc854ce821c30cf
AyaIbrahim3027 <ayaibrahim3027@gmail.com>
2024-04-22 10:59:28 +0200

    Add State Renderer Implementation Part 2

--- lib/presentation/common/state_renderer/state_renderer.dart
+++ lib/presentation/common/state_renderer/state_renderer.dart
@@ -19,8 +19,8 @@ class StateRenderer extends StatelessWidget {
   const StateRenderer(
       {super.key,
       required this.stateRendererType,
-       this.message = AppStrings.loading,
-       this.title = '',
+      this.message = AppStrings.loading,
+      this.title = '',
       required this.retryActionFunction});
 
   final StateRendererType stateRendererType;
@@ -31,5 +31,30 @@ class StateRenderer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
+  }
+
+  Widget _getStateWidget() {
+    switch (stateRendererType) {
+      case StateRendererType.popupLoadingState:
+      // TODO: Handle this case.
+      case StateRendererType.popupErrorState:
+      // TODO: Handle this case.
+      case StateRendererType.fullScreenLoadingState:
+      // TODO: Handle this case.
+      case StateRendererType.fullScreenErrorState:
+      // TODO: Handle this case.
+      case StateRendererType.fullScreenEmptyState:
+      // TODO: Handle this case.
+      case StateRendererType.contentState:
+      // TODO: Handle this case.
+    }
+  }
+
+  Widget _getItemsColumn(List<Widget> children) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: children,
+    );
   }
 }
