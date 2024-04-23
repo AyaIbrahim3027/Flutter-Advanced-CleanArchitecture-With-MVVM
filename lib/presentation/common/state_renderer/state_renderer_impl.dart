965bcebcf154a63fa49a3d045f527056f987980e
AyaIbrahim3027 <ayaibrahim3027@gmail.com>
2024-04-23 11:23:44 +0200

    Adding Extension on State Renderer implementer

--- lib/presentation/common/state_renderer/state_renderer_impl.dart
+++ lib/presentation/common/state_renderer/state_renderer_impl.dart
@@ -1,6 +1,7 @@
 import 'package:advanced_flutter/app/constants.dart';
 import 'package:advanced_flutter/presentation/common/state_renderer/state_renderer.dart';
 import 'package:advanced_flutter/presentation/resources/strings_manager.dart';
+import 'package:flutter/cupertino.dart';
 
 abstract class FlowState {
   StateRendererType getStateRendererType();
@@ -43,7 +44,6 @@ class ErrorState extends FlowState {
 // content state
 
 class ContentState extends FlowState {
-
   ContentState();
 
   @override
@@ -64,6 +64,21 @@ class EmptyState extends FlowState {
   String getMessage() => message;
 
   @override
-  StateRendererType getStateRendererType() => StateRendererType.fullScreenEmptyState;
+  StateRendererType getStateRendererType() =>
+      StateRendererType.fullScreenEmptyState;
 }
 
+extension FlowStateExtension on FlowState {
+  Widget getScreenWidget(BuildContext context, Widget contentScreenWidget,
+      Function retryActionFunction) {
+    switch(runtimeType){
+      case LoadingState:
+
+      case ErrorState:
+      case EmptyState:
+      case ContentState:
+      default:
+
+    }
+  }
+}
