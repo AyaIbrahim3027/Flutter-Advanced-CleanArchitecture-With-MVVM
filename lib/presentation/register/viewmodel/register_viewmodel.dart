0609523db3f2be6a0b0ef7b319171cbd90a94ff2
AyaIbrahim3027 <ayaibrahim3027@gmail.com>
2024-04-25 13:01:03 +0200

    Add Register ViewModel Input and Output

--- lib/presentation/register/viewmodel/register_viewmodel.dart
+++ lib/presentation/register/viewmodel/register_viewmodel.dart
@@ -33,3 +33,27 @@ class RegisterViewModel extends BaseViewModel {
     super.dispose();
   }
 }
+
+abstract mixin class RegisterViewModelInput {
+  Sink get inputUserName;
+  Sink get inputMobileNumber;
+  Sink get inputEmail;
+  Sink get inputPassword;
+  Sink get inputProfilePicture;
+}
+
+abstract mixin class RegisterViewModelOutput {
+  Stream<bool> get outputIsUserNameValid;
+  Stream<String?> get outputErrorUserName;
+
+  Stream<bool> get outputIsMobileNumberValid;
+  Stream<String?> get outputErrorMobileNumber;
+
+  Stream<bool> get outputIsEmailValid;
+  Stream<String?> get outputErrorEmail;
+
+  Stream<bool> get outputIsPasswordValid;
+  Stream<String?> get outputErrorPassword;
+
+  Stream<bool> get outputIsProfilePictureValid;
+}
