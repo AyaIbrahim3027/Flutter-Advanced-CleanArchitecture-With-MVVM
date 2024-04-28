1e049326b3820d0817af05114b0ad0a37b920146
AyaIbrahim3027 <ayaibrahim3027@gmail.com>
2024-04-28 11:09:44 +0200

    Finalising Add Pages inside Main View

--- lib/presentation/main/main_view.dart
+++ lib/presentation/main/main_view.dart
@@ -2,8 +2,11 @@ import 'package:advanced_flutter/presentation/main/pages/home_page.dart';
 import 'package:advanced_flutter/presentation/main/pages/notifications_page.dart';
 import 'package:advanced_flutter/presentation/main/pages/search_page.dart';
 import 'package:advanced_flutter/presentation/main/pages/settings_page.dart';
+import 'package:advanced_flutter/presentation/resources/color_manager.dart';
 import 'package:advanced_flutter/presentation/resources/strings_manager.dart';
+import 'package:advanced_flutter/presentation/resources/values_manager.dart';
 import 'package:flutter/material.dart';
+
 class MainView extends StatefulWidget {
   const MainView({super.key});
 
@@ -12,24 +15,70 @@ class MainView extends StatefulWidget {
 }
 
 class _MainViewState extends State<MainView> {
-  List<Widget> pages =[
+  List<Widget> pages = [
     const HomePage(),
     const SearchPage(),
     const NotificationsPage(),
     const SettingsPage(),
   ];
 
+  List<String> titles = [
+     AppStrings.home,
+    AppStrings.search,
+    AppStrings.notifications,
+    AppStrings.settings,
+  ];
+
   var _title = AppStrings.home;
   var _currentIndex = 0;
 
-
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       appBar: AppBar(
-        title: Text(_title,style: Theme.of(context).textTheme.titleSmall,),
+        title: Text(
+          _title,
+          style: Theme.of(context).textTheme.titleSmall,
+        ),
       ),
       body: pages[_currentIndex],
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+          boxShadow: [
+            BoxShadow(color: ColorManager.black, spreadRadius: AppSize.s1_5)
+          ],
+        ),
+        child: BottomNavigationBar(
+            selectedItemColor: ColorManager.primary,
+            unselectedItemColor: ColorManager.grey,
+            currentIndex: _currentIndex,
+            onTap: onTap,
+            items: const [
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home_outlined),
+                label: AppStrings.home,
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.search),
+                label: AppStrings.search,
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.notifications_outlined),
+                label: AppStrings.notifications,
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.settings_outlined),
+                label: AppStrings.home,
+              ),
+            ]),
+      ),
     );
+  }
+
+  onTap(int index) {
+    setState(() {
+      _currentIndex = index;
+      _title = _title[index];
+    });
   }
 }
