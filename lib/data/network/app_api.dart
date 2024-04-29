4dabbfa44104d10bd1e5ad2edc3d0ed10508b30d
AyaIbrahim3027 <ayaibrahim3027@gmail.com>
2024-04-29 12:24:20 +0200

    Add Home API Inside App Service Client

--- lib/data/network/app_api.dart
+++ lib/data/network/app_api.dart
@@ -25,6 +25,9 @@ abstract class AppServiceClient {
     @Field("password") String password,
     @Field("profile_picture") String profilePicture,
   );
+
+  @GET('/home')
+  Future<HomeResponse> getHomeData();
 }
 
 
