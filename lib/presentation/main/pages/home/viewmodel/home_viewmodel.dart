829a89b30bf414260369d88f1dfa7794e2672de2
AyaIbrahim3027 <ayaibrahim3027@gmail.com>
2024-04-30 12:12:33 +0200

    Add Home ViewModel Inputs and Outputs

--- lib/presentation/main/pages/home/viewmodel/home_viewmodel.dart
+++ lib/presentation/main/pages/home/viewmodel/home_viewmodel.dart
@@ -6,7 +6,8 @@ import 'package:rxdart/rxdart.dart';
 import '../../../../../domain/usecase/home_usecase.dart';
 import '../../../../base/base_view_model.dart';
 
-class HomeViewModel extends BaseViewModel {
+class HomeViewModel extends BaseViewModel
+    with HomeViewModelInput, HomeViewModelOutput {
   final StreamController _bannersStreamController =
       BehaviorSubject<List<BannerAd>>();
   final StreamController _servicesStreamController =
@@ -17,6 +18,8 @@ class HomeViewModel extends BaseViewModel {
   final HomeUseCase homeUseCase;
 
   HomeViewModel(this.homeUseCase);
+
+  // inputs
 
   @override
   void start() {
@@ -30,4 +33,41 @@ class HomeViewModel extends BaseViewModel {
     _storesStreamController.close();
     super.dispose();
   }
+
+  @override
+  Sink get inputBanners => _bannersStreamController.sink;
+
+  @override
+  Sink get inputServices => _servicesStreamController.sink;
+
+  @override
+  Sink get inputStores => _storesStreamController.sink;
+
+  // outputs
+
+  @override
+  Stream<List<BannerAd>> get outputBanners =>
+      _bannersStreamController.stream.map((banners) => banners);
+
+  @override
+  // TODO: implement outputServices
+  Stream<List<Service>> get outputServices =>
+      _servicesStreamController.stream.map((services) => services);
+
+  @override
+  // TODO: implement outputStores
+  Stream<List<Store>> get outputStores =>
+      _storesStreamController.stream.map((stores) => stores);
+}
+
+abstract mixin class HomeViewModelInput {
+  Sink get inputStores;
+  Sink get inputServices;
+  Sink get inputBanners;
+}
+
+abstract mixin class HomeViewModelOutput {
+  Stream<List<Store>> get outputStores;
+  Stream<List<Service>> get outputServices;
+  Stream<List<BannerAd>> get outputBanners;
 }
